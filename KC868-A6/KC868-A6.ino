826cb731e813b9ec6a603f7ccc4fb8f0aefdf9de
alastaira <alastair.aitchison@gmail.com>
2025-01-26 17:12:16 +0000

    Updates.

--- KC868-A6/KC868-A6.ino
+++ KC868-A6/KC868-A6.ino
@@ -18,6 +18,7 @@ nRF24L01:
 CE: 22
 */
 // INCLUDES
+#include <WiFi.h>
 // I2C
 #include <Wire.h>
 // See https://github.com/LennartHennigs/Button2
@@ -122,11 +123,49 @@ void onPress(Button2& btn) {
 void onRelease(Button2& btn) {
 }
 
+void readMacAddress(){
+  // Variable to store the MAC address
+  uint8_t baseMac[6];
+  
+  // Get MAC address of the WiFi station interface
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  Serial.print("Station MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+  
+  // Get the MAC address of the Wi-Fi AP interface
+  esp_read_mac(baseMac, ESP_MAC_WIFI_SOFTAP);
+  Serial.print("SoftAP MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+  
+  // Get the MAC address of the Bluetooth interface
+  esp_read_mac(baseMac, ESP_MAC_BT);
+  Serial.print("Bluetooth MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+
+  // Get the MAC address of the Ethernet interface
+  esp_read_mac(baseMac, ESP_MAC_ETH);
+  Serial.print("Ethernet MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+}
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println(F("Kincony KC868-A6 Setup"));
 
+  readMacAddress();
 
   pinMode(DAC2, OUTPUT);
 
